069fc2b044777471313865f8c8c6ab3bc998dff2
Mazid Ahmad <mazidahmad@Stockbit-MA.local>
2022-07-01 16:43:38 +0700

    feat(macos): Add support macos configuration

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import secure_enclave
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SecureEnclavePlugin.register(with: registry.registrar(forPlugin: "SecureEnclavePlugin"))
 }
