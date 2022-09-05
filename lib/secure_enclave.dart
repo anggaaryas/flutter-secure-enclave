dafb1935d238235cf0db5368e868e08b258b17af
Angga Saputra v2 <angga.saputra@coinbit.id>
2022-09-05 13:26:33 +0700

    add singleton to access platform code

--- lib/secure_enclave.dart
+++ lib/secure_enclave.dart
@@ -1,5 +1,11 @@
 library secure_enclave;
 
+import 'dart:typed_data';
+
+import 'package:secure_enclave/secure_enclave_base.dart';
+import 'package:secure_enclave/src/models/access_control_model.dart';
+import 'package:secure_enclave/src/models/result_model.dart';
+
 import 'src/platform/secure_encalve_swift.dart';
 
 export 'src/constants/access_control_option.dart';
@@ -7,4 +13,52 @@ export 'src/models/access_control_model.dart';
 export 'src/models/result_model.dart';
 export 'src/models/error_model.dart';
 
-class SecureEnclave extends SecureEnclaveSwift {}
+class SecureEnclave implements SecureEnclaveBase {
+
+
+  @override
+  Future<ResultModel<String?>> decrypt({required Uint8List message, required String tag, String? password}) {
+    return SecureEnclavePlatform.instance.decrypt(message: message, tag: tag);
+  }
+
+  @override
+  Future<ResultModel<Uint8List?>> encrypt({required String message, required String tag, String? password}) {
+    return SecureEnclavePlatform.instance.encrypt(message: message, tag: tag);
+  }
+
+  @override
+  Future<ResultModel<Uint8List?>> encryptWithPublicKey({required String message, required String publicKey}) {
+    return SecureEnclavePlatform.instance.encryptWithPublicKey(message: message, publicKey: publicKey);
+  }
+
+  @override
+  Future<ResultModel<bool>> generateKeyPair({required AccessControlModel accessControl}) {
+    return SecureEnclavePlatform.instance.generateKeyPair(accessControl: accessControl);
+  }
+
+  @override
+  Future<ResultModel<String?>> getPublicKey({required String tag, String? password}) {
+    return SecureEnclavePlatform.instance.getPublicKey(tag: tag);
+  }
+
+  @override
+  Future<ResultModel<bool?>> getStatusSecKey({required String tag, String? password}) {
+    return SecureEnclavePlatform.instance.getStatusSecKey(tag: tag);
+  }
+
+  @override
+  Future<ResultModel<bool>> removeKey(String tag) {
+    return SecureEnclavePlatform.instance.removeKey(tag);
+  }
+
+  @override
+  Future<ResultModel<String?>> sign({required Uint8List message, required String tag, String? password}) {
+    return SecureEnclavePlatform.instance.sign(message: message, tag: tag);
+  }
+
+  @override
+  Future<ResultModel<bool?>> verify({required String plainText, required String signature, required String tag, String? password}) {
+    return SecureEnclavePlatform.instance.verify(plainText: plainText, signature: signature, tag: tag);
+  }
+
+}
--- lib/src/platform/secure_encalve_swift.dart
+++ lib/src/platform/secure_encalve_swift.dart
@@ -2,11 +2,37 @@ import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import '../../secure_enclave_base.dart';
 import '../models/access_control_model.dart';
 import '../models/result_model.dart';
 
-class SecureEnclaveSwift extends SecureEnclaveBase {
+
+
+abstract class SecureEnclavePlatform extends PlatformInterface implements SecureEnclaveBase {
+  /// Constructs a SecureEnclavePlatform.
+  SecureEnclavePlatform() : super(token: _token);
+
+  static final Object _token = Object();
+
+  static SecureEnclavePlatform _instance = SecureEnclaveSwift();
+
+  /// The default instance of [SecureEnclavePlatform] to use.
+  ///
+  /// Defaults to [MethodChannelSecureEnclave].
+  static SecureEnclavePlatform get instance => _instance;
+
+  /// Platform-specific implementations should set this with their own
+  /// platform-specific class that extends [SecureEnclavePlatform] when
+  /// they register themselves.
+  static set instance(SecureEnclavePlatform instance) {
+    PlatformInterface.verifyToken(instance, _token);
+    _instance = instance;
+  }
+}
+
+
+class SecureEnclaveSwift extends SecureEnclavePlatform {
   /// The method channel used to interact with the native platform.
   @visibleForTesting
   final methodChannel = const MethodChannel('secure_enclave');
