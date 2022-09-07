6c702fd072a24759ce8e1dc23e4f0c7b24587b9b
Angga Saputra v2 <angga.saputra@coinbit.id>
2022-09-07 14:58:45 +0700

    add integration test

---
+++ example/integration_test/integration_test.dart
@@ -0,0 +1,119 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:secure_enclave/secure_enclave.dart';
+
+void main(){
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  const String tagNormal = "app.privateKey";
+  const String tagBiometric = "app.privateKey.biometric";
+  const String tagPassword = "app.privateKey.password";
+  const String tagPasswordBiometric = "app.privateKey.password.biometric";
+
+  const String appPassword = "1234";
+
+  testWidgets("create Key", (widgetTester) async {
+
+    blankApp("Test create key");
+    await widgetTester.pumpAndSettle();
+
+    SecureEnclave secureEnclave = SecureEnclave();
+    await secureEnclave.generateKeyPair(accessControl: AccessControlModel(tag: tagNormal, options: [AccessControlOption.privateKeyUsage])).then((result){
+      checkResult(
+          result: result,
+          onSuccess: (){
+            print('Tag Normal created...');
+            expect(result.value, true);
+          });
+    });
+
+    await secureEnclave.generateKeyPair(accessControl: AccessControlModel(tag: tagBiometric, options: [AccessControlOption.privateKeyUsage, AccessControlOption.biometryCurrentSet])).then((result){
+      checkResult(
+          result: result,
+          onSuccess: (){
+            print('Tag Biometry created...');
+            expect(result.value, true);
+          });
+    });
+
+    await secureEnclave.generateKeyPair(accessControl: AccessControlModel(tag: tagPassword, options: [AccessControlOption.privateKeyUsage, AccessControlOption.applicationPassword], password: appPassword)).then((result){
+      checkResult(
+          result: result,
+          onSuccess: (){
+            print('Tag Password created...');
+            expect(result.value, true);
+          });
+    });
+
+    await secureEnclave.generateKeyPair(accessControl: AccessControlModel(tag: tagPasswordBiometric, options: [AccessControlOption.privateKeyUsage, AccessControlOption.biometryCurrentSet, AccessControlOption.applicationPassword], password: appPassword)).then((result){
+      checkResult(
+          result: result,
+          onSuccess: (){
+            print('Tag Biometry Password created...');
+            expect(result.value, true);
+          });
+    });
+
+  });
+
+  group('Normal Encrypt Decrypt', () {
+    const String cleartext = "Lorem Ipsum";
+    Uint8List? encrypted;
+
+    testWidgets('normal encrypt', (widgetTester) async {
+
+      blankApp("Test normal encrypt...");
+      await widgetTester.pumpAndSettle();
+
+      SecureEnclave secureEnclave = SecureEnclave();
+      await secureEnclave.encrypt(message: cleartext, tag: tagNormal).then((result){
+        checkResult(result: result, onSuccess: (){
+          encrypted = result.value;
+
+          expect(encrypted != null, true);
+          expect(encrypted!.isEmpty, false);
+        });
+      });
+    });
+
+
+    testWidgets("normal decrypt", (widgetTester) async{
+
+      if(encrypted == null || encrypted!.isEmpty){
+        throw("Encrypted Text null or empty. abort...");
+      }
+
+      blankApp("Test normal decrypt...");
+      await widgetTester.pumpAndSettle();
+
+      SecureEnclave secureEnclave = SecureEnclave();
+      await secureEnclave.decrypt(message: encrypted!, tag: tagNormal).then((result) {
+        checkResult(result: result, onSuccess: (){
+          expect(result.value == cleartext, true);
+        });
+      });
+    });
+
+  });
+}
+
+void checkResult({required ResultModel result, required Function() onSuccess}) {
+  if(result.error == null){
+    onSuccess();
+  } else {
+    throw(result.error!.desc);
+  }
+}
+
+void blankApp(String title){
+  runApp(MaterialApp(
+    home: Container(
+      color: Colors.white,
+      child: Center(
+        child: Text(title, style: TextStyle(fontSize: 24),),
+      ),
+    ),
+  ));
+}
